04dda6c5f55f2bbe7372c7a09213cbf71be4a2af
Aradhya Nepal <aradhya.1221@gmail.com>
2024-11-12 12:55:58 +0545

    Finishing websocket research

--- Backend/src/main/java/com/a2/pickyami/gameplay/WebSocketEventListener.java
+++ Backend/src/main/java/com/a2/pickyami/gameplay/WebSocketEventListener.java
@@ -1,8 +1,11 @@
 package com.a2.pickyami.gameplay;
 
+import com.a2.pickyami.gameplay.model.GameStartModel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -10,9 +13,15 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
-
+private  final SimpMessageSendingOperations messageTemplate;
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
-
+        StompHeaderAccessor headerAccessor=StompHeaderAccessor.wrap(event.getMessage());
+        String username=(String) headerAccessor.getSessionAttributes().get("username");
+        if(username!=null){
+            log.info("User disconnected: {}",username);
+            var gameResponse= GameStartModel.builder().type(MessageType.LEAVE).sender(username).build();
+            messageTemplate.convertAndSend("topic/public",gameResponse);
+        }
     }
 }
--- Backend/src/main/java/com/a2/pickyami/gameplay/model/GameStartModel.java
+++ Backend/src/main/java/com/a2/pickyami/gameplay/model/GameStartModel.java
@@ -1,13 +1,17 @@
 package com.a2.pickyami.gameplay.model;
 
+import com.a2.pickyami.gameplay.MessageType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class GameStartModel {
      private String message;
      private String sender;
+     private MessageType type;
 }
