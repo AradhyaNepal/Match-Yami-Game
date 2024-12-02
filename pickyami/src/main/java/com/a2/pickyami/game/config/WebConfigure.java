d4889e27353b8fd520b9a6500353db677da4dea7
Aradhya Nepal <aradhya.1221@gmail.com>
2024-12-02 21:54:19 +0545

    In /websocket server gets an request which crashes

--- pickyami/src/main/java/com/a2/pickyami/game/config/WebConfigure.java
+++ pickyami/src/main/java/com/a2/pickyami/game/config/WebConfigure.java
@@ -1,19 +1,24 @@
-package com.a2.pickyami.game.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-public class WebConfigure implements WebSocketConfigurer {
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(gameHandler(),"/game").setAllowedOrigins("*").setHandshakeHandler();
-
-    }
-
-    @Bean
-    public WebSocketHandler gameHandler(){
-        return  new CustonWebSocketHandler() ;
-    }
-}
+//package com.a2.pickyami.game.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.web.socket.WebSocketHandler;
+//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+//
+//public class WebConfigure implements WebSocketConfigurer {
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(gameHandler(), "/game").setAllowedOrigins("*").setHandshakeHandler(handshakeHandler());
+//
+//    }
+//
+//    @Bean
+//    public WebSocketHandler gameHandler() {
+//        return new CustonWebSocketHandler();
+//    }
+//
+//    @Bean
+//    public CustomHandshakeHandler handshakeHandler() {
+//        return new CustomHandshakeHandler();
+//    }
+//}
--- pickyami/src/main/java/com/a2/pickyami/game/config/WebSocketConfiguration.java
+++ pickyami/src/main/java/com/a2/pickyami/game/config/WebSocketConfiguration.java
@@ -1,34 +1,34 @@
-//package com.a2.pickyami.game.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.event.EventListener;
-//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-//import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-//import org.springframework.web.socket.messaging.SessionConnectedEvent;
-//
-//@Configuration
-//@EnableWebSocketMessageBroker
-//public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/websocket")
-//                .set(new CustomHandshakeHandler())
-//                .setAllowedOrigins("*")
-//                .withSockJS(); // WebSocket endpoint
-//    }
-//
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.enableSimpleBroker("/queue", "/topic","/user/queue/start");
-//        registry.setUserDestinationPrefix("/user");
-//        registry.setApplicationDestinationPrefixes("/ws");
-//    }
-//
-//    @EventListener
-//    void handleSessionConnectedEvent(SessionConnectedEvent event) {
-//        StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
-//    }
-//}
+package com.a2.pickyami.game.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket")
+                .setHandshakeHandler(new CustomHandshakeHandler())
+                .setAllowedOrigins("*")
+                .withSockJS(); // WebSocket endpoint
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/queue", "/topic","/user/queue/start");
+        registry.setUserDestinationPrefix("/user");
+        registry.setApplicationDestinationPrefixes("/ws");
+    }
+
+    @EventListener
+    void handleSessionConnectedEvent(SessionConnectedEvent event) {
+        StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
+    }
+}
